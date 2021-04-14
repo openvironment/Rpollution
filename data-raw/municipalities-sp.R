b4683be9720cdd4692e65312fdb18258f9430f56
Beatriz Milz <beatriz.milz@hotmail.com>
2021-04-14 12:53:35 -0300

    update variable name in municipalities_sp

--- data-raw/municipalities-sp.R
+++ data-raw/municipalities-sp.R
@@ -2,13 +2,13 @@ municipalities_sp <-  geobr::read_municipality("SP") %>%
   sf::st_drop_geometry() %>%
   dplyr::select(code_muni, name_muni) %>%
   tibble::as_tibble() %>%
-  dplyr::mutate(muni = stringr::str_to_upper(name_muni),
-                muni = abjutils::rm_accent(muni),
-                muni = stringr::str_replace_all(muni, "-", " "),
-                muni = stringr::str_replace_all(muni,  "'", " "),
-                muni = dplyr::case_when(muni == "MOJI MIRIM" ~
+  dplyr::mutate(municipality = stringr::str_to_upper(name_muni),
+                municipality = abjutils::rm_accent(municipality),
+                municipality = stringr::str_replace_all(municipality, "-", " "),
+                municipality = stringr::str_replace_all(municipality,  "'", " "),
+                municipality = dplyr::case_when(municipality == "MOJI MIRIM" ~
                                           "MOGI MIRIM",
-                                        TRUE ~ muni))
+                                        TRUE ~ municipality))
 
 
 usethis::use_data(municipalities_sp, overwrite = TRUE)
