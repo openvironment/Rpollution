146dd5c0c3b0518eaac18474a4114d943614b9fa
Beatriz Milz <beatriz.milz@hotmail.com>
2021-04-14 15:31:27 -0300

    change name of repo

--- R/RPollution.R
+++ R/RPollution.R
@@ -3,7 +3,7 @@
 #' Functions for air pollution analysis
 #'
 #' See the README
-#' on \href{https://github.com/williamorim/Rpollution#readme}{GitHub}
+#' on \href{https://github.com/openvironment/Rpollution#readme}{GitHub}
 #'
 #' @docType package
 #' @name Rpollution
@@ -11,7 +11,40 @@
 NULL
 
 ## quiets concerns of R CMD check re: the variables that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(
-  c("X1", "X2", "X3", "conc", "hour", "stationname"),
-  add = FALSE
-)
+if (getRversion() >= "2.15.1")
+  utils::globalVariables(
+    c(
+      "X1",
+      "X2",
+      "X3",
+      "conc",
+      "hour",
+      "stationname",
+      "altitude",
+      "data",
+      "data_coleta",
+      "fim_operacao",
+      "hora_coleta id",
+      "infoaguas_station_ids",
+      "inicio_operacao",
+      "initial_date",
+      "lat_graus",
+      "lat_min",
+      "lat_sec",
+      "latitude",
+      "long_graus",
+      "long_min",
+      "long_sec",
+      "longitude",
+      "periodo_ate",
+      "periodo_de",
+      "valor",
+      "valor_numerico",
+      "valor_texto",
+      "hora_coleta",
+      "id"
+    )
+    ,
+    add = FALSE
+
+  )
