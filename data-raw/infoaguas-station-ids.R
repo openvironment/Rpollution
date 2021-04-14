6609fbb6b19b19ab7b2a709765486ecc93e3eee8
Beatriz Milz <beatriz.milz@hotmail.com>
2021-04-14 13:54:21 -0300

    remove data from municipalities and add in infoaguas-station-ids

--- data-raw/infoaguas-station-ids.R
+++ data-raw/infoaguas-station-ids.R
@@ -56,6 +56,19 @@ id_table <-
     "id_station" = cod_ponto
   )
 
+municipalities_sp <-  geobr::read_municipality("SP") %>%
+  sf::st_drop_geometry() %>%
+  dplyr::select(code_muni, name_muni) %>%
+  tibble::as_tibble() %>%
+  dplyr::mutate(municipality = stringr::str_to_upper(name_muni),
+                municipality = abjutils::rm_accent(municipality),
+                municipality = stringr::str_replace_all(municipality, "-", " "),
+                municipality = stringr::str_replace_all(municipality,  "'", " "),
+                municipality = dplyr::case_when(municipality == "MOJI MIRIM" ~
+                                                  "MOGI MIRIM",
+                                                TRUE ~ municipality))
+
+
 infoaguas_station_ids <-  id_table %>%
   dplyr::left_join(municipalities_sp, by = "municipality") %>%
   dplyr::select(-municipality) %>%
